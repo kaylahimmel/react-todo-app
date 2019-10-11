c82d82b6bfe37d0eba2b5f6fcffe318b1ecc4000
Kayla Himmelberger <kaylahimmelberger@MacBook-Pro-3.local>
2019-10-11 12:54:49 -0400

    added checkbox toggle functionality

--- to-do-tutorial/src/App.js
+++ to-do-tutorial/src/App.js
@@ -1,19 +1,39 @@
 import React from 'react';
-import './styles/styles.css';
 import Header from '../src/components/Header'
 import Footer from '../src/components/Footer'
 import ListItem from './components/ListItem'
 import todoData from './todoData'
+import './styles/styles.css';
 
 class App extends React.Component {
-  render() {
-    const taskComponent = todoData.map(task => {
-      return (
-        <div>
-          <ListItem key={task.id} task={task} />
-        </div>
-      )
+  constructor() {
+    super()
+    this.state = {
+      todos: todoData
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(id) {
+    this.setState(prevState => {
+      const updatedList = prevState.todos.map(task => {
+        if (task.id === id) {
+          task.completed = !task.completed
+        } 
+        return {
+          task
+        }
+      })
+      return {
+        todo: updatedList
+      }
     })
+  }
+
+  render() {
+    const taskComponent = this.state.todos.map(task => 
+      <ListItem key={task.id} task={task} handleChange={this.handleChange} />
+    )
   
     return (
       <div className="App">
--- to-do-tutorial/src/components/ListItem.js
+++ to-do-tutorial/src/components/ListItem.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
-function ListItem (props) {
+function ListItem(props) {
   
   return (
-    <div className="list-item">
-      <input type='checkbox' checked={props.task.completed} />
+    <div 
+      className="list-item"
+    >
+      <input 
+        type='checkbox' 
+        checked={props.task.completed} 
+        onChange={() => props.handleChange(props.task.id)}
+      />
       <p>{props.task.text}</p>
     </div>
   )
